0b65657583dda991e850d6e97550bb99b31d920f
Ebuube <onwutaebubegideon1555@gmail.com>
2026-07-16 18:56:15 +0000

    chore(hashtable): add comment description for `hash_djb2()` function

--- src/hashtable/hash_djb2.c
+++ src/hashtable/hash_djb2.c
@@ -1,5 +1,12 @@
 #include "hashtable.h"
 
+/**
+ * hash_djb2 - compute hash using Dnaiel J. Bernstein's algorithm
+ * @str: string to hash
+ *
+ * Description: does not fail when str is upto 1000 chars long, order affects hash value
+ * Return: hash value
+ */
 unsigned long hash_djb2(const char *str)
 {
 	unsigned long hash = 5381;	/* Daniel J. Bernstein's magic number */
